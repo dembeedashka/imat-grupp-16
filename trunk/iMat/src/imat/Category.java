d47aa462137977a2c1d68a8404b94ab627c81902
ocklind@student.chalmers.se <ocklind@student.chalmers.se@8554e6ec-9038-5de6-5c69-318797080b1f>
2011-03-02 09:41:28 +0000

    Colors fixed

--- trunk/iMat/src/imat/Category.java
+++ trunk/iMat/src/imat/Category.java
@@ -9,13 +9,13 @@ import java.awt.Color;
 public class Category {
 
     /* Category colors */
-    public static final Color HOME_WHITE      = Color.WHITE;
-    public static final Color FRUIT_GREEN     = new Color(0, 0, 0);
-    public static final Color MEAT_RED        = new Color(0, 0, 0);
-    public static final Color DAIRY_BLUE      = new Color(0, 0, 0);
-    public static final Color PANTRY_YELLOW   = new Color(0, 0, 0);
-    public static final Color SNACKS_ORANGE   = new Color(0, 0, 0);
-    public static final Color FAVOURITE_BROWN = new Color(0, 0, 0);
+    public static final Color HOME_WHITE       = Color.WHITE;
+    public static final Color FRUIT_GREEN      = new Color(143, 228, 139);
+    public static final Color MEAT_RED         = new Color(228, 139, 139);
+    public static final Color DAIRY_BLUE       = new Color(139, 194, 228);
+    public static final Color PANTRY_YELLOW    = new Color(228, 227, 139);
+    public static final Color SNACKS_ORANGE    = new Color(234, 168, 82);
+    public static final Color FAVOURITES_BROWN = new Color(143, 110, 67);
 
     private Color   color;
     private String  card;
